d6371c430a9805d728a368f36feeaf5d84a86183
Valentina.Pro <verano86@mail.ru>
2024-03-27 20:53:32 +0300

    New test added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,11 +5,23 @@ import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxArrayEnds() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxArrayMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 47, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 47;
 
         long actual = service.findMax(incomesInBillions);
 
